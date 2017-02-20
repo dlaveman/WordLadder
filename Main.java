332d41333820b2b5992e7ecbf55e106c8941e66b
dlaveman <dlaveman@utexas.edu>
2017-02-20 15:27:00 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+	public static ArrayList<String> ladder;
 	// static variables and constants only here.
 	
 	public static void main(String[] args) throws Exception {
@@ -30,11 +30,16 @@ public class Main {
 			kb = new Scanner(new File(args[0]));
 			ps = new PrintStream(new File(args[1]));
 			System.setOut(ps);			// redirect output to ps
-		} else {
+		} 
+		else {
 			kb = new Scanner(System.in);// default from Stdin
 			ps = System.out;			// default to Stdout
 		}
 		initialize();
+		ladder=parse(kb);
+		if(ladder!=null){
+		getWordLadderDFS(ladder.get(0),ladder.get(2));
+		}
 		
 		// TODO methods to read in words, output ladder
 	}
@@ -43,22 +48,32 @@ public class Main {
 		// initialize your static variables or constants here.
 		// We will call this method before running our JUNIT tests.  So call it 
 		// only once at the start of main.
+			
 	}
 	
 	/**
 	 * @param keyboard Scanner connected to System.in
-	 * @return ArrayList of 2 Strings containing start word and end word. 
+	 * @return ArrayList of Strings containing start word, rungs, and end word. 
 	 * If command is /quit, return empty ArrayList. 
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
-		// TO DO
-		return null;
+		ArrayList<String> parseLadder=new ArrayList<String>();
+		String word=keyboard.next();
+		if(word.equals("/quit")){
+			return null;
+		}
+		else{
+			parseLadder.add(word);
+			parseLadder.add("2");
+			parseLadder.add(keyboard.next());
+		}
+		return parseLadder;
 	}
 	
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
 		
 		// Returned list should be ordered start to end.  Include start and end.
-		// Return empty list if no ladder.
+		// If ladder is empty, return list with just start and end.
 		// TODO some code
 		Set<String> dict = makeDictionary();
 		// TODO more code
